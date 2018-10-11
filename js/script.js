fc826fec85cb8a8d2a9633c498b73ee7f306a3f2
DerianW <ScratchGobo@gmail.com>
2018-10-11 09:20:22 -0400

    more st00f

--- js/script.js
+++ js/script.js
@@ -1,2 +1,5 @@
 'use strict'
 document.getElementById("wooder").innerHTML = "wooder (the drink)" ;
+
+function toCelsius(fahrenheit) { return (5/9) * (fahrenheit-32) ; }
+document.getElementById("con").innerHTML = toCelsius(77); 
